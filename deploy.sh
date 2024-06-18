47e53ff1fe84dcac61e7b72319f941540c1c7bd9
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-06-18 18:26:00 +0100

    feat: multiline deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-VERSION=$(git describe --tags --abbrev=0 | tr -d '\n') GIT_COMMIT=$(git rev-parse HEAD) BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') goreleaser release --snapshot --clean
+VERSION=$(git describe --tags --abbrev=0 | tr -d '\n') \
+GIT_COMMIT=$(git rev-parse HEAD) \
+BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+goreleaser release --snapshot --clean
