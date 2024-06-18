8ade8522839180f37de12598bfaa01587ebf2de6
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-06-18 18:33:24 +0100

    feat: multiline install script

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ GIT_COMMIT="$(git rev-parse HEAD)"
 VERSION="$(git describe --tags --abbrev=0 | tr -d '\n')"
 echo "${BUILD_DATE} ${GIT_COMMIT} ${VERSION}"
 
-go build -o dj -ldflags="-X 'github.com/ashish10alex/dj/internal/version.buildDate=${BUILD_DATE}' -X 'github.com/ashish10alex/dj/internal/version.gitCommit=${GIT_COMMIT}' -X 'github.com/ashish10alex/dj/internal/version.gitVersion=${VERSION}'" .
+go build -o bin/dj \
+-ldflags="-X 'github.com/ashish10alex/dj/internal/version.buildDate=${BUILD_DATE}' \
+-X 'github.com/ashish10alex/dj/internal/version.gitCommit=${GIT_COMMIT}' \
+-X 'github.com/ashish10alex/dj/internal/version.gitVersion=${VERSION}'"
 
