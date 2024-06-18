6a33cb6c529f0b6ad372665900d7784884c7f736
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-06-18 23:44:32 +0100

    feat: release attempt

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 VERSION=$(git describe --tags --abbrev=0 | tr -d '\n') \
 GIT_COMMIT=$(git rev-parse HEAD) \
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
-goreleaser release --snapshot --clean
+goreleaser release
