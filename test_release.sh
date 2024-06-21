07158356d4489aa57853bbc818716e1cb9e7177c
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-06-21 17:52:39 +0100

    chore: update test deployment script

--- test_release.sh
+++ test_release.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+goreleaser check
+
 VERSION=$(git describe --tags --abbrev=0 | tr -d '\n') \
 GIT_COMMIT=$(git rev-parse HEAD) \
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
-goreleaser release
+goreleaser release --snapshot --clean
